22b50f82bd3550a8f21920ebdfcdb1a79ca364ac
dpdahal <dp48dahal@gmail.com>
2024-06-23 07:12:26 +0545

    update

---
+++ javascript-course/day3/js/custom.js
@@ -0,0 +1,53 @@
+// document.title = "Day 3";
+// document.write('<h1>Day 3</h1>');
+// myusername
+// myUserName
+// document.getElementById("demo").innerText="<h1>Day 3</h1>";
+// document.getElementById("demo").innerHTML="<h1>Day 3</h1>";
+
+// let box = document.getElementsByClassName("box");
+// console.log(box);
+
+// box.forEach((element) => {
+//     element.style.backgroundColor = "red";
+// });
+
+// document.getElementsByTagName("h2");
+// console.log(document.getElementsByTagName("h2"));
+
+// let box =document.querySelector(".box");
+// let box=document.querySelectorAll(".box");
+// console.log(box);
+
+// let demo = document.querySelector("#demo");
+// console.log(demo);
+
+// let demo = document.querySelector("#test");
+
+// let demo = document.getElementById("test");
+// demo.innerHTML = "<h1>Day 3</h1>";
+// console.log(demo);
+
+// attribute selector
+// let demo = document.querySelectorAll("[type='text']");
+// console.log(demo);
+// demo.forEach((element) => {
+//     element.value = "Hello";
+// });
+// console.log(demo);
+
+
+
+
+// setTimeout(()=>{
+//     document.getElementById("demo").style.background="green";
+// },3000)
+
+// document.getElementById("num1").value=0;
+
+
+function demo(){
+    let num1 = document.getElementById("num1").value;
+    console.log(typeof num1);
+
+}
